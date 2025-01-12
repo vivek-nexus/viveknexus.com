b736aa6973eb2333dac2f3bee7ff87d01e60f2ee
vivek-nexus <audit536@gmail.com>
2025-01-12 21:04:16 +0530

    Add mouseflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { SUSE } from "next/font/google"
 import "./globals.css"
+import Script from "next/script"
 
 const suse = SUSE({
   subsets: ["latin"]
@@ -19,6 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script>{`
+        window._mfq = window._mfq || [];
+        (function() {
+          var mf = document.createElement("script");
+          mf.type = "text/javascript"; mf.defer = true;
+          mf.src = "//cdn.mouseflow.com/projects/d650c614-2064-44cc-804a-54644c37dd52.js";
+          document.getElementsByTagName("head")[0].appendChild(mf);
+        })();
+      `}</Script>
       <body
         className={`${suse.className} antialiased bg-black text-[#C0C0C0]`}
       >
