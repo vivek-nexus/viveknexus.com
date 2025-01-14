22c1d05af15f3e74d2d2a8bad323b9a01d58641c
vivek-nexus <audit536@gmail.com>
2025-01-14 19:51:32 +0530

    Update testimonials.ts

--- src/constants/testimonials.ts
+++ src/constants/testimonials.ts
@@ -1,16 +1,16 @@
 export const testimonials = [
     {
-        quote: "I don't like lorem epsum, but for now, I'll live with it. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat eros id eros vulputate hendrerit. Ut eu aliquam augue. Mauris sapien  lacus, auctor et nibh vel, feugiat laoreet sapien. Sed vestibulum ex  dolor, eu placerat nibh molestie ac. Pellentesque ut lacus sed sem  pretium pharetra a et est.",
+        quote: "I don't like lorem epsum, but until I add this content, I'll let it be ;) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat eros id eros vulputate hendrerit. Ut eu aliquam augue. Mauris sapien  lacus, auctor et nibh vel, feugiat laoreet sapien. Sed vestibulum ex  dolor, eu placerat nibh molestie ac. Pellentesque ut lacus sed sem  pretium pharetra a et est.",
         name: "Vishwas",
-        title: "Chief Nerd at Nomad"
+        title: "Chief Nerd at Boring Inc"
     },
     {
-        quote: "I don't like lorem epsum, but for now, I'll live with it. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat eros id eros vulputate hendrerit. Ut eu aliquam augue. Mauris sapien  lacus, auctor et nibh vel, feugiat laoreet sapien. Sed vestibulum ex  dolor, eu placerat nibh molestie ac.",
+        quote: "I don't like lorem epsum, but until I add this content, I'll let it be ;) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat eros id eros vulputate hendrerit. Ut eu aliquam augue. Mauris sapien  lacus, auctor et nibh vel, feugiat laoreet sapien. Sed vestibulum ex  dolor, eu placerat nibh molestie ac.",
         name: "Pooja",
-        title: "Senior Designer at Fantasy Land"
+        title: "Senior Designer at Deno"
     },
     {
-        quote: "I don't like lorem epsum, but for now, I'll live with it. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat eros id eros vulputate hendrerit. Ut eu aliquam augue. Mauris sapien  lacus, auctor et nibh vel, feugiat laoreet sapien. Sed vestibulum ex  dolor, eu placerat nibh molestie ac. Pellentesque ut lacus sed sem  pretium pharetra a et est. Praesent in est nisi. Praesent tincidunt erat lectus, id hendrerit lorem rhoncus vel.",
+        quote: "I don't like lorem epsum, but until I add this content, I'll let it be ;) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat eros id eros vulputate hendrerit. Ut eu aliquam augue. Mauris sapien  lacus, auctor et nibh vel, feugiat laoreet sapien. Sed vestibulum ex  dolor, eu placerat nibh molestie ac. Pellentesque ut lacus sed sem  pretium pharetra a et est. Praesent in est nisi. Praesent tincidunt erat lectus, id hendrerit lorem rhoncus vel.",
         name: "John",
         title: "Software Developer at Lala land"
     },
